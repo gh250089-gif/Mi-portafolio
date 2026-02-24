d7f98d67bf8398ca3507cd10806084ed0d952231
Guillermo <gh250089@gmail.com>
2026-02-23 22:57:00 -0600

    guardar cambios

---
+++ script.js
@@ -0,0 +1,12 @@
+alert('ESTA ES LA PRIMERA PRUEBA DE ALERTA');
+alert('segunda prueba de alerta');
+ 
+
+
+
+
+
+console.log(' Hello, World! '.trim()); // Prints Hello, World!
+console.log('Hello'.length); // Prints 5
+
+ 
