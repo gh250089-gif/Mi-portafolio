e9976cd8424e4adb0e1d022928039788ed908142
Guillermo Hernandez <gh250089@gmail.com>
2026-02-25 22:14:54 -0600

    Subiendo mi portafolio

---
+++ cv/cv/script.js
@@ -0,0 +1,7 @@
+
+
+
+console.log(' Hello, World! '.trim()); // Prints Hello, World!
+console.log('Hello'.length); // Prints 5
+
+ 
--- cv/script.js
+++ cv/script.js
@@ -1,4 +1,9 @@
+alert('ESTA ES LA PRIMERA PRUEBA DE ALERTA');
+alert('segunda prueba de alerta');
+ 
 
+
+console.log('Hello'.length);
 
 
 console.log(' Hello, World! '.trim()); // Prints Hello, World!
